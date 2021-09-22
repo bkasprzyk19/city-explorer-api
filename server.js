e312558a74bc264648567ab78ce9e1f952cd6fad
Brian Kasprzyk <bkasprzyk@outlook.com>
2021-09-21 17:09:48 -0700

    file error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const climates = require('./data.weather.json');
+const climates = require('./data/weather.json');
 
 
 const PORT = process.env.PORT || 3001;
