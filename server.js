7653710cc57a8ffc40ea23f2349b34e88161a8c5
Brian Kasprzyk <bkasprzyk@outlook.com>
2021-09-22 23:51:00 -0700

    working1

--- server.js
+++ server.js
@@ -2,33 +2,51 @@
 
 
 require('dotenv').config();
-
 const express = require('express');
+
 const cors = require('cors');
-const climates = require('./data/weather.json');
+const axios = require('axios');
+
 
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
-
-// middleware
 app.use(cors());
 
 
 app.get('/', (request, response) => {
-  response.status(200).send('I am home at last!');
+  response.status(200).send('Authenticated, Grade A Prime');
 });
 
-app.get('/climates', (request, response) => {
+function Forecast(day) {
+  this.day = day.valid_date;
+  this.description = day.weather.description;
+}
 
-  const city_name = request.query.city_name === "true";
+app.get('/weather', async (request, response) => {
 
-  const firstClimate = climates.find(climate => climate.searchQuery === city_name);
+  
+  const city_name = request.query.city_name;
+  const lat = request.query.lat;
+  const lon = request.query.lon;
 
-  response.send(firstClimate);
+  const weatherAPI = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.REACT_APP_WEATHER_API_KEY}&land=en&lat=${lat}&lon=${lon}&days=5`
 
-})
+
+
+
+
+
+
+  if (firstClimate) {
+    const weatherArr = currentForecast.data.map(item => new Forecast(item));
+    response.send(weatherArr);
+  } else {
+    response.status(555).send('submit a valid city name')
+  }
+ 
+
+});
 
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
